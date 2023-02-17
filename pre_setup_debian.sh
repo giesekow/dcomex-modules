9c4ff5b0dc76f27e47999dd09b1b1bfd66d4c057
Giles Ekow Tetteh <giles.tetteh@tum.de>
2023-02-17 21:10:07 +0100

    removed broken link

--- pre_setup_debian.sh
+++ pre_setup_debian.sh
@@ -14,5 +14,6 @@ tar xjf ilastik-1.*-Linux.tar.bz2
 mv ilastik-1.*-Linux/* "$script_dir/ilastik/src/"
 rm -rf ilastik-1.*-Linux
 chmod +x "$script_dir/ilastik/src/run_ilastik.sh"
+rm "$script_dir/ilastik/src/lib/libGurobiJni*.so"
 
 cd $cwd
